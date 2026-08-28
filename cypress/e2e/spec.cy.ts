d46e43b9236a970f87b14df7cbb739fef02365a4
Alexandre Denoyel <denoyela@gmail.com>
2026-08-28 09:47:12 +0200

    feat(tests): remove obsolete authentication flow and login page tests

--- cypress/e2e/spec.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('Initial navigation', () => {
-  it('redirects the root path to the login page', () => {
-    cy.visit('/')
-    cy.url().should('include', '/login')
-    cy.contains('Login Form')
-  })
-})
